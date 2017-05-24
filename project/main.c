febb6d1f59a674e49dc24055b4173180fac881ee
Karl Palsson <karlp@tweak.net.au>
2017-05-24 22:45:47 +0000

    scons: add git version info to #defines
    does it via the command line for compat with the project this is a demo
    for....

--- project/main.c
+++ project/main.c
@@ -3,8 +3,13 @@
 #include "shared.h"
 #include "options.h"
 
-int main(void) {
+#define STR1(x) #x
+#define STR(x) STR1(x)
 
+int main(void) {
+	printf("App version (total): %s\n", STR(VERSION_STRING));
+	printf("  major.minor: %d.%d\n", VERSION_MAJOR, VERSION_MINOR);
+	printf("  and is %s\n", VERSION_CLEAN ? "clean" : "dirty");
 	int count = hw_get_count();
 	shared_thing(count);
 	printf("All builds print option: %d\n", OPTION_somewild);
